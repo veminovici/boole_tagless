bd9bd0a3a0ab6ed7438ef6a3b3ebc03b57d963a0
VLAD EMINOVICI <veminovici@hotmail.com>
2025-03-31 08:14:55 +0300

    Evaluation - final style evaluation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 
 mod tagless00;
+mod tagless01;
 
 pub use tagless00::*;
+pub use tagless01::*;
---
+++ src/tagless01.rs
@@ -0,0 +1,24 @@
+type Repr = i32;
+
+pub fn lit(n: i32) -> Repr {
+    n
+}
+
+pub fn neg(n: Repr) -> Repr {
+    -n
+}
+
+pub fn add(n1: Repr, n2: Repr) -> Repr {
+    n1 + n2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_eval() {
+        let e = add(lit(1), lit(2));
+        assert_eq!(e, 3);
+    }
+}
